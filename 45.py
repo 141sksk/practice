47d9ccb54f6410103689e8e74f5c6e9656f24b39
141sksk <14sk.13puto@gmail.com>
2019-06-22 18:04:40 +0900

    0621

---
+++ 45.py
@@ -0,0 +1,19 @@
+def main():
+
+    N = int(input())
+    sushi_plates = list(map(int,input().split()))
+
+    if N == 1:
+        print(sushi_plates.pop())
+        exit()
+
+    else:
+        answers = [0] * (N + 1)
+        answers[1] = sushi_plates[0]
+        for i in range(2, N + 1):
+            answers[i] = max(answers[i - 1], answers[i - 2] + sushi_plates[i - 1])
+
+    print(answers[N])
+
+if __name__ == '__main__':
+    main()
