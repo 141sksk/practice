a77e647595f4167b1ad9edb7beba827f55e5ee19
141sksk <14sk.13puto@gmail.com>
2019-08-05 23:21:24 +0900

    0805

---
+++ yukicoder/561.py
@@ -0,0 +1,15 @@
+def main():
+    N, D = map(int, input().split())
+    Tk = 0
+    Kt = -D
+    for _ in range(N):
+        t, k = map(int, input().split())
+        newTk = max(Tk + t, Kt + t - D)
+        newKt = max(Kt + k, Tk + k - D)
+        Tk = newTk
+        Kt = newKt
+
+    print(max(Tk, Kt))
+
+if __name__ == "__main__":
+    main()
